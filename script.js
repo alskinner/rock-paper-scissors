732e478ed8842781cbc68e286afa73f119c24805
alskinner <115374241+alskinner@users.noreply.github.com>
2023-03-02 14:55:16 -0700

    UPDATE script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerSelection;
+let computerSelection = getComputerChoice();
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice = "";
@@ -17,4 +21,36 @@ function getComputerChoice() {
     }
 
     return computerChoice
+}
+
+function playRound(playerSelection, computerSelection) {
+    let roundResult = "";
+
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            roundResult = "You Lose! Paper beats rock";
+        } else if (computerSelection === "Scissors") {
+            roundResult = "You Win! Rock beats scissors";
+        } else {
+            roundResult = "It's a tie!"
+        }
+    }   else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            roundResult = "You Win! Paper beats rock";
+        } else if (computerSelection === "Scissors") {
+            roundResult = "You Lose! Scissors beats paper";
+        } else {
+            roundResult = "It's a tie!"
+        }
+    }   else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            roundResult = "You Lose! Rock beats scissors";
+        } else if (computerSelection === "Paper") {
+            roundResult = "You Win! Scissors beats paper";
+        } else {
+            roundResult = "It's a tie!"
+        }
+    }
+
+    return roundResult;   
 }
